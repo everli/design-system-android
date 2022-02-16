2dc7658daaa4c57c50dbdf9850d4c724a782fa24
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2022-02-16 12:32:46 +0200

    Update TokensPlayground.kt

--- playground/src/main/java/com/everli/designsystem/playground/theme/TokensPlayground.kt
+++ playground/src/main/java/com/everli/designsystem/playground/theme/TokensPlayground.kt
@@ -3,6 +3,8 @@ package com.everli.designsystem.playground.theme
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +16,10 @@ fun TokensPlayground() {
   // ColorsList(colors = tokenColors())
   Column(
     verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally) {
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = Modifier
+      .fillMaxHeight()
+      .fillMaxWidth()) {
     Text("\uD83D\uDEA7 Work in progress \uD83D\uDEA7", modifier = Modifier)
   }
 }
